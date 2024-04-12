d4959a214f76f4afcf720ee19621de3ec3cbedb2
mikus <139052997+cappuch@users.noreply.github.com>
2024-04-12 19:04:49 +0100

    Update and rename infer.py to infer_cli.py

--- infer_cli.py
+++ infer_cli.py
@@ -1,4 +1,6 @@
+import argparse
 import base64
+import json
 from PIL import Image
 import numpy as np
 import torch
@@ -34,16 +36,35 @@ def infer_single_image(image_bytes, model):
     return rotation_pred.item(), l2_pred.item(), r2_pred.item()
 
 def main():
-    model = Model(num_classes=3)
-    model_path = 'pytorch_model.pth'
-    model.load_state_dict(torch.load(model_path))
+    parser = argparse.ArgumentParser(description='Infer image with trained model')
+    parser.add_argument('image_path', type=str, help='Path to the input image')
+    parser.add_argument('--save_to_file', action='store_true', help='Save predictions to a JSON file')
+    parser.add_argument('--load_file', type=str, help='Path to the model file')
+    args = parser.parse_args()
 
-    image_bytes = 'images.jpeg'
+    if args.load_file:
+        model = Model(num_classes=3)
+        model.load_state_dict(torch.load(args.load_file))
+    else:
+        model = Model(num_classes=3)
+        model_path = 'pytorch_model.pth'
+        model.load_state_dict(torch.load(model_path))
+
+    image_bytes = args.image_path
     rotation, l2, r2 = infer_single_image(image_bytes, model)
 
     print("Predicted Rotation:", rotation)
     print("Predicted L2 Value:", l2)
     print("Predicted R2 Value:", r2)
+
+    if args.save_to_file:
+        predictions = {
+            'rotation': rotation,
+            'l2': l2,
+            'r2': r2
+        }
+        with open('predictions.json', 'w') as f:
+            json.dump(predictions, f)
 
 if __name__ == "__main__":
     main()
